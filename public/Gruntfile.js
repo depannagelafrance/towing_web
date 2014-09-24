93d02c65e1abf7d342c4fc608770b9d72a9c5e55
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2014-09-24 16:42:20 +0200

    Add grunt file to preform watch and build tasks

---
+++ public/Gruntfile.js
@@ -0,0 +1,109 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    watch: {
+      options: {
+        livereload: true
+      },
+      sass: {
+        files: ['assets/sass/{,**/}*.{scss,sass}'],
+        tasks: ['compass:dev'],
+        options: {
+          livereload: false
+        }
+      },
+      images: {
+        files: ['assets/images/**']
+      },
+      css: {
+        files: ['assets/stylesheets/{,**/}*.css']
+      },
+      scripts: {
+        files: ['assets/scripts/{,**/}*.js', '!assets/scripts/{,**/}*.min.js'],
+        tasks: ['jshint', 'uglify:dev']
+      }
+    },
+
+    compass: {
+      options: {
+        config: 'config.rb',
+        bundleExec: true,
+        force: true
+      },
+      dev: {
+        options: {
+          environment: 'development'
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production'
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['assets/scripts/{,**/}*.js', '!assets/scripts/{,**/}*.min.js']
+    },
+
+    uglify: {
+      dev: {
+        options: {
+          mangle: false,
+          compress: false,
+          beautify: true
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'assets/scripts',
+          dest: 'assets/scripts',
+          src: ['**/*.js', '!**/*.min.js'],
+          rename: function(dest, src) {
+            var folder = src.substring(0, src.lastIndexOf('/'));
+            var filename = src.substring(src.lastIndexOf('/'), src.length);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            return dest + '/' + folder + filename + '.min.js';
+          }
+        }]
+      },
+      dist: {
+        options: {
+          mangle: true,
+          compress: true
+        },
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'assets/scripts',
+          dest: 'assets/scripts',
+          src: ['**/*.js', '!**/*.min.js'],
+          rename: function(dest, src) {
+            var folder = src.substring(0, src.lastIndexOf('/'));
+            var filename = src.substring(src.lastIndexOf('/'), src.length);
+            filename = filename.substring(0, filename.lastIndexOf('.'));
+            return dest + '/' + folder + filename + '.min.js';
+          }
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('build', [
+    'uglify:dist',
+    'compass:dist',
+    'jshint'
+  ]);
+
+};
