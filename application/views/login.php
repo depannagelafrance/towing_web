006fc15493cb862d9da5e3be91a38277fe17b564
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2014-09-24 16:36:18 +0200

    remove table markup from login

--- application/views/login.php
+++ application/views/login.php
@@ -3,15 +3,17 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('/login/perform') ?>
-  <table border="0">
-      <tr>
-          <td>Login:</td>
-          <td><input type="text" value="<?php echo set_value('login'); ?>" name="login" /></td>
-      </tr>
-      <tr>
-          <td>Wachtwoord:</td>
-          <td><input type="password" value="" name="password" /></td>
-      </tr>
-      <tr><td colspan="2"><input type="submit" name="submit" value="Log in!" /></td></tr>
-  </table>
+ <div>
+   <label>Login:</label>
+   <input type="text" value="<?php echo set_value('login'); ?>" name="login" />
+ </div>
+
+  <div>
+    <label>Wachtwoord:</label>
+    <input type="password" value="" name="password" />
+  </div>
+
+  <div>
+    <input type="submit" name="submit" value="Log in!" /></td></tr>
+  </div>
 </form>
