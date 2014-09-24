4cb90e218c318eb4817fd2c91ba2f408d68fc9ad
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2014-09-24 16:40:09 +0200

    add .rb config file and gemset to use ruby version manager (rvm) and bundler to manage gems

---
+++ public/config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+require 'compass-normalize'
+require 'rgbapng'
+require 'toolkit'
+require 'susy'
+require 'sass-globbing'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/scripts"
+fonts_dir = "assets/fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
