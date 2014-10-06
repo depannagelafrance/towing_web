d3a6ddb1c25830561adf0e15abc97494e5109088
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-10-06 20:33:41 +0200

    Depot model

---
+++ application/models/Depot_model.php
@@ -0,0 +1,25 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once(APPPATH . '/models/Depot_model.php');
+
+class Depot_model  {
+  public $id              = null;
+  public $name            = null;
+  public $street          = null;
+  public $street_number   = null;
+  public $street_pobox    = null;
+  public $zip             = null;
+  public $city            = null;
+
+  public function __construct($data = null) {
+    if($data) {
+      $this->id             = $data->id;
+      $this->name           = $data->name;
+      $this->street         = $data->street;
+      $this->street_number  = $data->street_number;
+      $this->street_pobox   = $data->street_pobox;
+      $this->zip            = $data->zip;
+      $this->city           = $data->city;
+    }
+  }
+}
