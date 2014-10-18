e2e9d58300ca2698d6050b50556b38a724357337
Gert van den Buijs <gertvandenbuijs@hotmail.com>
2014-10-18 22:36:47 +0200

    nice iconzzzz

--- application/views/fast_dossier/dossier.php
+++ application/views/fast_dossier/dossier.php
@@ -17,7 +17,7 @@ $_dossier = $dossier->dossier;
 
       $this->load->helper('date');
 
-      $this->table->set_heading('Dossier', 'Datum', 'Tijd');
+      $this->table->set_heading('Dossier');
 
       //d.id, d.id as 'dossier_id', t.id as 'voucher_id', d.call_number, d.call_date, t.voucher_number, ad.name 'direction_name',
       //adi.name 'indicator_name', c.code as `towing_service`, ip.name as `incident_type`
@@ -26,7 +26,7 @@ $_dossier = $dossier->dossier;
         foreach($vouchers as $voucher) {
 
           if($voucher->dossier_number === $url_dossier_id){
-            $class = 'active';
+            $class = 'active bright';
           }else{
             $class = 'inactive';
           }
@@ -36,9 +36,9 @@ $_dossier = $dossier->dossier;
             $prev = $voucher->dossier_number;
 
             $this->table->add_row(
-                  array('class' => $class, 'data' => sprintf('<a href="/fast_dossier/dossier/%s/%s">%s</a>', $voucher->dossier_number, $voucher->voucher_number, $voucher->dossier_number)), // $voucher->voucher_number),
-                  array('class' => $class, 'data' =>mdate('%d %M',strtotime($voucher->call_date))),
-                  array('class' => $class, 'data' =>mdate('%H:%i',strtotime($voucher->call_date)))
+                  array('class' => $class, 'data' => sprintf('<a class="id__cell" href="/fast_dossier/dossier/%s/%s"><span class="id__cell__icon icon--map"></span><span class="id__cell__text">%s</span></a>', $voucher->dossier_number, $voucher->voucher_number, $voucher->dossier_number)) // $voucher->voucher_number),
+                  //array('class' => $class, 'data' => mdate('%d %M',strtotime($voucher->call_date))),
+                  //array('class' => $class, 'data' => mdate('%H:%i',strtotime($voucher->call_date)))
             );
 
           }
