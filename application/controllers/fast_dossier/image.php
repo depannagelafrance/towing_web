df272fcea53e77e2526b15e05fe30798c1ae14e0
Kris Vandermast <kris.vandermast@ext.ec.europa.eu>
2014-10-24 21:04:45 +0200

    resampling signature

--- application/controllers/fast_dossier/image.php
+++ application/controllers/fast_dossier/image.php
@@ -11,23 +11,46 @@ class Image extends Page {
   /**
    * Index Page for this controller.
    */
-  public function view($id)
+  public function view($id, $w=250, $h=250)
   {
     $token = $this->_get_user_token();
 
     $f = $this->document_service->fetchDocumentById($id, $token);
 
-    header('Pragma: public');     // required
-    header('Expires: 0');         // no cache
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Last-Modified: '.gmdate ('D, d M Y H:i:s', mktime()).' GMT');
-    header('Cache-Control: private',false);
-    header('Content-Type: '.$f->content_type);  // Add the mime type from Code igniter.
-    //header('Content-Disposition: attachment; filename="'.$report->filename.'"');  // Add the file name
-    //header('Content-Transfer-Encoding: binary');
-    header('Content-Length: '.$f->file_size); // provide file size
-    //header('Connection: close');
-    print base64_decode($f->data);
+    $_image_stream = base64_decode($f->data);
+
+    $image = imagecreatefromstring($_image_stream);
+
+    if($image !== false) {
+      header('Pragma: public');     // required
+      header('Expires: 0');         // no cache
+      header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+      header('Last-Modified: '.gmdate ('D, d M Y H:i:s', mktime()).' GMT');
+      header('Cache-Control: private',false);
+      header('Content-Type: '.$f->content_type);  // Add the mime type from Code igniter.
+
+      $width = imagesx($image);
+      $height = imagesy($image);
+
+      $ratio = max($h / $height, $w / $width);
+
+      $new_height = ceil($height * $ratio);
+      $new_width = ceil($width * $ratio);
+
+      // Resample
+      $image_p = imagecreatetruecolor($new_width, $new_height);
+      imagealphablending($image_p, false);
+      imagesavealpha($image_p,true);
+      $transparent = imagecolorallocatealpha($image_p, 255, 255, 255, 127);
+      imagefilledrectangle($image_p, 0, 0, $new_width, $new_height, $transparent);
+
+      imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+      imagepng($image_p);
+      imagedestroy($image);
+      imagedestroy($image_p);
+
+    }
 
     exit();
 
