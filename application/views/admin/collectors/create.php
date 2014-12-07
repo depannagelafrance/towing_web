c2ee7953d4ff754dfb3b3cdf99fcac1a609d37cc
gert van den buijs <gert.vandenbuijs@digipolis.be>
2014-12-07 13:36:44 +0100

    collectors

--- application/views/admin/collectors/create.php
+++ application/views/admin/collectors/create.php
@@ -1,23 +1,23 @@
-<div class="box unpadded dsform" style="width:850px">
-  <div class="inner_padding">
+<?php
 
-    <div style="width: 800px; margin: auto;">
-      <?php
-        //TODO: @Gert, voorzien van een correcte styling voor de form errors!
+$errors = validation_errors();
 
-        $errors = validation_errors();
+if($errors) {
+    printf('<div style="background: red; color: white; font-size: 1.2em; padding-top:10px; padding-bottom: 10px; padding-left: 4px;">%s</div>', $errors);
+}
+?>
+<?php echo form_open('/admin/collector/create')?>
 
-        if($errors) {
-          printf('<div style="background: red; color: white; font-size: 1.2em; padding-top:10px; padding-bottom: 10px; padding-left: 4px;">%s</div>', $errors);
-        }
-      ?>
+<div class="box unpadded dsform admin_form">
+    <div class="inner_padding">
+        <h2 class="admin_form_title">Afhaler aanmaken</h2>
 
-      <?php echo form_open('/admin/collector/create')?>
         <div class="form-item">
     	     <input type="text" placeholder="Name"
     		     value="<?php print set_value('name'); ?>" name="name" />
         </div>
-
+    </div>
+    
         <div class="form__actions">
           <div class="form__actions__cancel">
             <div class="form-item">
@@ -30,7 +30,6 @@
             </div>
           </div>
         </div>
-      <?php echo form_close();?>
-    </div>
-  </div>
 </div>
+
+<?php echo form_close();?>
--- application/views/admin/collectors/edit.php
+++ application/views/admin/collectors/edit.php
@@ -1,20 +1,27 @@
-e<?php echo form_open('admin/collector/edit/'.$id)?>
-<div class="box layout_2col_container">
-  <div class="layout_2col_item">
-    <?php echo validation_errors(); ?>
+<?php echo form_open('admin/collector/edit/'.$id)?>
+<?php echo validation_errors(); ?>
+<div class="box unpadded dsform admin_form">
+    <div class="inner_padding">
+        <h2 class="admin_form_title">Afhaler bewerken</h2>
 
-    <div class="form-item-horizontal">
-        <label>Naam: </label>
-        <?php print form_input('name', $name); ?>
+        <div class="form-item-horizontal">
+            <label>Naam: </label>
+            <?php print form_input('name', $name); ?>
+        </div>
     </div>
 
-    <div class="form-item-horizontal">
-      <input type="submit" name="submit" value="Opslaan" />
-    </div>
+      <div class="box form__actions">
+          <div class="form__actions__cancel">
+              <div class="form-item">
+                  <a href="/admin/collector">Annuleren</a>
+              </div>
+          </div>
+          <div class="form__actions__save">
+              <div class="form-item">
+                  <input type="submit" value="Bewaren" name="submit">
+              </div>
+          </div>
+      </div>
 
-    <div class="form-item-horizontal">
-      <a href="/admin/collector">Annuleren</a>
-    </div>
-  </div>
 </div>
-  <?php echo form_close();?>
+<?php echo form_close();?>
