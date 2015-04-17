23359a25f8f76863ab01453570b8927e9dcc5d2d
kvandermast <kvandermast@gmail.com>
2015-04-17 20:30:59 +0200

    fixed decoding issue with document download

--- application/controllers/fast_dossier/Document.php
+++ application/controllers/fast_dossier/Document.php
@@ -16,26 +16,32 @@ class Document extends Page {
 
         $token = $this->_get_user_token();
         $document = $this->document_service->fetchDocumentById($id, $token);
-
+        
         header('Pragma: public');     // required
         header('Expires: 0');         // no cache
         header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Last-Modified: '.gmdate ('D, d M Y H:i:s', mktime()).' GMT');
+        header('Last-Modified: '.gmdate ('D, d M Y H:i:s', time()).' GMT');
         header('Cache-Control: private',false);
         header('Content-Type: '.$document->content_type);  // Add the mime type from Code igniter.
         header('Content-Disposition: attachment; filename="'.$document->name.'"');  // Add the file name
         header('Content-Transfer-Encoding: binary');
         //header('Content-Length: '.filesize($path)); // provide file size
         header('Connection: close');
-
-        $data = explode(',', $document->data); //splitting of the "data:image/jpeg;base64,"" stuff
-
-        if(count($data) > 1) {
-          print base64_decode($data[1]);
+        
+        $data = base64_decode($document->data);
+ 
+        if(substr($data, 0,strlen("data:image/jpeg;base64,")) === "data:image/jpeg;base64,") {
+          $data = explode(',', $data);
+          print $data[1];
         } else {
-          print base64_decode($document->data);
+	  if(substr($document->data, 0, strlen("data:")) === "data:") {
+             $data = explode(',', $document->data);
+             print base64_decode($data[1]);
+          } else {
+             print $data;
+          }
         }
 
-        exit();
+        die();
     }
 }
