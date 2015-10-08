f91e62c6e54d95fc7f17a68d54ca366807767b29
Kris Vandermast <kvandermast@gmail.com>
2015-10-08 12:47:50 +0200

    removed short tags

--- application/views/fast_dossier/dossier.php
+++ application/views/fast_dossier/dossier.php
@@ -1,3 +1,23 @@
-<?php phpinfo(); ?>
-<?php die(); ?>
+<?php
+function displayVoucherTimeField($value, $name)
+{
+    if ($value)
+    {
+        $render = sprintf('<div class="input_value">%s</div>', asTime($value));
+        $render .= form_hidden($name, asJsonDateTime($value));
 
+        return $render;
+    }
+    else
+    {
+        return form_input($name, asTime($value));
+    }
+}
+
+
+$this->load->helper('listbox');
+$this->load->helper('datetime');
+$this->load->helper('date');
+
+$_dossier = $dossier->dossier;
+?>
